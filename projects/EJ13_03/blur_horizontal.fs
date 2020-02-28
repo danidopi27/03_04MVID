5d16407f3d12ddef6d4c1db3cf3974e8638aa912
Daniel Dopico Graña <danieldopicograna@gmail.com>
2020-02-28 19:14:57 +0100

    Ejercicio 13_03 (Error en shader vert. y horiz.)

---
+++ projects/EJ13_03/blur_horizontal.fs
@@ -0,0 +1,39 @@
+/*/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  CURSO:	   M�ster en Dise�o y Desarrollo de Videojuegos
+  ASIGNATURA:  Programaci�n I
+  DESCRIPCI�N: blur_horizontal.fs del ejercicio 13_03
+  PROGRAMADOR: Daniel Dopico Gra�a
+  FECHA:       Febrero 2020
+  VERSI�N:     1.0
+
+*//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////C�DIGO//////////////////////////////////////////////////////////////////////
+#version 330 core
+
+out vec4 FragColor;
+
+in vec2 uv;
+
+uniform sampler2D screenTexture;
+
+const float offset = 600.0;
+
+void main() {
+   float offsets[3] = (0.0, 1.3846153846, 3.2307692308);
+   float kernel[3] = (0.2270270270, 0.3162162162, 0.0702702703);
+
+   vec3 color = vec3(0.0);
+
+   color = vec3(texture(screenTexture, uv.st / 600)) * kernel[0];
+
+    for (int i = 1; i < 3; ++i) {
+        color += vec3(texture(screenTexture, (uv.st + vec2(offsets[i], 0.0) / offset))) * kernel[i];
+        color += vec3(texture(screenTexture, (uv.st - vec2(offsets[i], 0.0) / offset))) * kernel[i];
+    }
+
+    FragColor = vec4(color, 1.0);
+}
+
+////////////////////////////////////////////////////////////////////FIN DE C�DIGO//////////////////////////////////////////////////////////////////
---
+++ projects/EJ13_03/fbo.fs
@@ -0,0 +1,50 @@
+/*/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  CURSO:	   M�ster en Dise�o y Desarrollo de Videojuegos
+  ASIGNATURA:  Programaci�n I
+  DESCRIPCI�N: fbo.fs del ejercicio 13_02
+  PROGRAMADOR: Daniel Dopico Gra�a
+  FECHA:       Febrero 2020
+  VERSI�N:     1.0
+
+*//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////C�DIGO//////////////////////////////////////////////////////////////////////
+#version 330 core
+
+out vec4 FragColor;
+
+in vec2 uv;
+
+uniform sampler2D screenTexture;
+
+const float offset = 1.0 / 600.0;
+
+void main() {
+    vec2 offsets[9] = vec2[](
+        vec2(-offset, offset),
+        vec2(0.0, offset),
+        vec2(offset, offset),
+        vec2(-offset, 0.0),
+        vec2(0.0, 0.0),
+        vec2(offset, 0.0),
+        vec2(-offset, -offset),
+        vec2(0.0, -offset),
+        vec2(offset, -offset)
+    );
+
+    float kernel[9] = float[] (
+        1, 1, 1,
+        1,  -8, 1,
+        1, 1, 1
+    );
+
+    vec3 color = vec3(0.0);
+    for (int i = 0; i < 9; ++i) {
+        color += vec3(texture(screenTexture, uv.st + offsets[i])) * kernel[i];
+    }
+
+    FragColor = vec4(color, 1.0);
+}
+
+////////////////////////////////////////////////////////////////////FIN DE C�DIGO//////////////////////////////////////////////////////////////////
