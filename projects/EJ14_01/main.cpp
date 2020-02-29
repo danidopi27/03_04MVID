b2f29f35dd8407a17e6e057fbaac77181a6365ae
Daniel Dopico Graña <danieldopicograna@gmail.com>
2020-02-29 17:33:14 +0100

    Ejercicio 14_01 (Error en shader)

--- projects/EJ14_01/main.cpp
+++ projects/EJ14_01/main.cpp
@@ -1,14 +1,300 @@
-/*/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+﻿/*/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-  CURSO:	   M�ster en Dise�o y Desarrollo de Videojuegos
-  ASIGNATURA:  Programaci�n I
-  DESCRIPCI�N: Ejercicio 14_01
-  PROGRAMADOR: Daniel Dopico Gra�a
+  CURSO:	   Máster en Diseño y Desarrollo de Videojuegos
+  ASIGNATURA:  Programación I
+  DESCRIPCIÓN: Ejercicio 14_01
+  PROGRAMADOR: Daniel Dopico Graña
   FECHA:       Febrero 2020
-  VERSI�N:     1.0
+  VERSIÓN:     1.0
 
 *//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-///////////////////////////////////////////////////////////////////////C�DIGO//////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////CÓDIGO//////////////////////////////////////////////////////////////////////
 
-////////////////////////////////////////////////////////////////////FIN DE C�DIGO////////////////////////////////////////////////////////////////////
+//--------------------------------------LIBRER�AS--------------------------------------//
+#include <glad/glad.h>						// Librería de manejo de GLAD
+#include <GLFW/glfw3.h>						// Librería de manejo de GLFW
+#include <glm/gtc/matrix_transform.hpp>		// Librería de transformaci�n de matrices
+
+#include "engine/camera.hpp"
+#include "engine/geometry/cube.hpp"			// Librería de manejo de geometr�a cubo
+#include "engine/input.hpp"					// Librería de manejo de inputs
+#include "engine/shader.hpp"				// Librería de manejo de shaders
+#include "engine/texture.hpp"				// Librería de manejo de texturas
+#include "engine/window.hpp"				// Librería de manejo de ventanas
+#include "engine/geometry/sphere.hpp"		// Librería de manejo de geometría esfera
+#include "engine/geometry/quad.hpp"			// Librería de manejo de geometría quad
+#include <iostream>							// Librería de funciones del sistema
+//--------------------------------------LIBRER�AS--------------------------------------//
+
+//----------------------------------------VARIABLES----------------------------------------//
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));		// Definici�n de la c�mara
+glm::vec3 lightPos(1.2f, 1.0f, 2.0f);			// Posición de la luz	
+glm::vec3 lightDir(0.0f, -1.0f, 0.0f);			// Dirección de la luz
+
+const uint32_t k_shadow_height = 1024;
+const uint32_t k_shadow_width = 1024;
+const float k_shadow_near = 1.0f;
+const float k_shadow_far = 7.5f;
+
+float lastFrame = 0.0f;							// Variable de �ltimo frame
+float lastX, lastY;								// Variable de �ltima coordenada x, y
+bool firstMouse = true;							// Variable de primer movimiento de rat�n
+//----------------------------------------VARIABLES----------------------------------------//
+
+//----------------------------------------FUNCI�N HANDLEINPUT----------------------------------------//
+void handleInput(float dt) {
+	// Funci�n que maneja las entradas
+
+	Input* input = Input::instance();								// Objeto input
+
+	// Si la tecla W est� pulsada
+	if (input->isKeyPressed(GLFW_KEY_W)) {
+		camera.handleKeyboard(Camera::Movement::Forward, dt);		// Movimiento hacia adelante
+	}
+	// Si la tecla S est� pulsada
+	if (input->isKeyPressed(GLFW_KEY_S)) {
+		camera.handleKeyboard(Camera::Movement::Backward, dt);		// Movimiento hacia atr�s
+	}
+	// Si la tecla A est� pulsada
+	if (input->isKeyPressed(GLFW_KEY_A)) {
+		camera.handleKeyboard(Camera::Movement::Left, dt);			// Movimiento hacia la izquierda
+	}
+	// Si la tecla D est� pulsada
+	if (input->isKeyPressed(GLFW_KEY_D)) {
+		camera.handleKeyboard(Camera::Movement::Right, dt);			// Movimiento hacia la derecha
+	}
+}
+//----------------------------------------FUNCI�N HANDLEINPUT----------------------------------------//
+
+//----------------------------------FUNCI�N ONKEYPRESS----------------------------------//
+void onKeyPress(int key, int action) {
+	// Funci�n que controla las pulsaciones de teclas
+
+	// Si se pulsa la tecla Q
+	if (key == GLFW_KEY_Q && action == GLFW_PRESS) {
+		Window::instance()->setCaptureMode(true);		// Se pone el modo captura ON
+	}
+	// Si se pulsa la tecla E
+	if (key == GLFW_KEY_E && action == GLFW_PRESS) {
+		Window::instance()->setCaptureMode(false);		// Se pone el modo captura OFF
+	}
+}
+//----------------------------------FUNCI�N ONKEYPRESS----------------------------------//
+
+//----------------------------------------FUNCI�N ONMOUSEMOVED----------------------------------------//
+void onMouseMoved(float x, float y) {
+	// Funci�n que controla el movimiento del rat�n
+
+	// Si es el primer movimiento del rat�n
+	if (firstMouse) {
+		firstMouse = false;								// Se realiz� primer movimiento de rat�n
+		lastX = x;										// Guardado de la posici�n de x
+		lastY = y;										// Guardado de la posici�n de y
+	}
+
+	const float xoffset = x - lastX;					// Offset en x (posici�n actual - anterior)
+	const float yoffset = lastY - y;					// Offset en x (anterior posici�n - actual)
+	lastX = x;											// Actualizaci�n de la posici�n anterior en x
+	lastY = y;											// Actualizaci�n de la posici�n anterior en y
+
+	camera.handleMouseMovement(xoffset, yoffset);		// Ajuste del movimiento de la c�mara en x, y
+}
+//----------------------------------------FUNCI�N ONMOUSEMOVED----------------------------------------//
+
+//----------------------------FUNCI�N ONSCROLLMOVED----------------------------//
+void onScrollMoved(float x, float y) {
+	// Funci�n que controla el movimiento scroll
+
+	camera.handleMouseScroll(y);		// Ajuste del movimiento de la c�mara 
+}
+//----------------------------FUNCI�N ONSCROLLMOVED----------------------------//
+
+//-----------------------------------------------------------FUNCIÓN CREATEFBO-----------------------------------------------------------//
+std::pair<uint32_t, uint32_t> createFBO() {
+    uint32_t fbo;
+    glGenFramebuffers(1, &fbo);
+    glBindFramebuffer(GL_FRAMEBUFFER, fbo);
+
+    uint32_t depthMap;
+    glGenTextures(1, &depthMap);
+    glBindTexture(GL_TEXTURE_2D, depthMap);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, k_shadow_width, k_shadow_height, 0, GL_DEPTH_COMPONENT, GL_FLOAT, nullptr);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
+    float borderColor[]{ 1.0f, 1.0f, 1.0f, 1.0f };
+    glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, borderColor);
+    glBindTexture(GL_TEXTURE_2D, 0);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, depthMap, 0);
+    glDrawBuffer(GL_NONE);
+    glReadBuffer(GL_NONE);
+
+    if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE) {
+        std::cout << "Framebuffer Incomplete" << std::endl;
+    }
+
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+    return std::make_pair(fbo, depthMap);
+}
+//-----------------------------------------------------------FUNCIÓN CREATEFBO-----------------------------------------------------------//
+
+//-------------------------------------------------------------------FUNCI�N RENDERSCENE-------------------------------------------------------------------//
+void renderScene(const Shader& s_phong, const Geometry& quad, const Geometry& cube, const Geometry& sphere,
+const Texture& t_albedo, const Texture& t_specular, const Shader& s_light, glm::mat4 view, glm::mat4 proj) {
+    t_albedo.use(s_phong, "material.diffuse", 0);
+    t_specular.use(s_phong, "material.specular", 1);
+
+    glm::mat4 model = glm::mat4(1.0);
+    model = glm::translate(model, glm::vec3(0.0f, -0.5f, 0.0f));
+    model = glm::rotate(model, glm::radians(-90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+    model = glm::scale(model, glm::vec3(10.0f, 10.0f, 10.0f));
+	s_phong.set("model", model);
+    glm::mat3 normalMat = glm::inverse(glm::transpose(glm::mat3(model)));
+	s_phong.set("normalMat", normalMat);
+    quad.render();
+
+    model = glm::mat4(1.0);
+	s_phong.set("model", model);
+    normalMat = glm::inverse(glm::transpose(glm::mat3(model)));
+	s_phong.set("normalMat", normalMat);
+    cube.render();
+
+    model = glm::mat4(1.0);
+    model = glm::translate(model, glm::vec3(0.0f, 2.0f, 0.0f));
+	s_phong.set("model", model);
+    normalMat = glm::inverse(glm::transpose(glm::mat3(model)));
+	s_phong.set("normalMat", normalMat);
+    sphere.render();
+
+	s_light.use();
+
+	model = glm::mat4(1.0);
+	model = glm::translate(model, lightPos);
+	model = glm::scale(model, glm::vec3(0.25f));
+	s_light.set("model", model);
+	s_light.set("view", view);
+	s_light.set("proj", proj);
+	s_light.set("lightColor", 1.0f, 1.0f, 1.0f);
+	sphere.render();
+}
+//-------------------------------------------------------------------FUNCI�N RENDERSCENE-------------------------------------------------------------------//
+
+//-------------------------------------------------------------------FUNCI�N RENDER-------------------------------------------------------------------//
+void render(const Geometry& quad, const Geometry& cube, const Geometry& sphere,
+    const Shader& s_phong, const Shader& s_depth, const Shader& s_debug, const Shader& s_light,
+    const Texture& t_albedo, const Texture& t_specular, const uint32_t fbo, const uint32_t fbo_texture) {
+
+//FIRST PASS
+    glBindFramebuffer(GL_FRAMEBUFFER, fbo);
+    glViewport(0, 0, k_shadow_width, k_shadow_height);
+    glClear(GL_DEPTH_BUFFER_BIT);
+    glEnable(GL_DEPTH_TEST);
+
+    const glm::mat4 lightProjection = glm::ortho(-10.0f, 10.0f, -10.0f, 10.0f, k_shadow_near, k_shadow_far);
+    const glm::mat4 lightView = glm::lookAt(lightPos, glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+    const glm::mat4 lightSpaceMatrix = lightProjection * lightView;
+
+    s_depth.use();
+    s_depth.set("lightSpaceMatrix", lightSpaceMatrix);
+    //glCullFace(GL_FRONT);
+    renderScene(s_depth, quad, cube, sphere, t_albedo, t_specular, s_light, lightView, lightProjection);
+    //glCullFace(GL_BACK);
+
+//SECOND PASS
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+    glViewport(0, 0, Window::instance()->getWidth(), Window::instance()->getHeight());
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glm::mat4 view = camera.getViewMatrix();
+    glm::mat4 proj = glm::perspective(glm::radians(camera.getFOV()), static_cast<float>(Window::instance()->getWidth()) / Window::instance()->getHeight(), 0.1f, 100.0f);
+
+    s_phong.use();
+
+    s_phong.set("view", view);
+    s_phong.set("proj", proj);
+
+    s_phong.set("viewPos", camera.getPosition());
+
+    s_phong.set("light.position", lightPos);
+	s_phong.set("light.direction", lightDir);
+    s_phong.set("light.ambient", 0.1f, 0.1f, 0.1f);
+    s_phong.set("light.diffuse", 0.5f, 0.5f, 0.5f);
+    s_phong.set("light.specular", 1.0f, 1.0f, 1.0f);
+
+	s_phong.set("light.constant", 1.0f);
+	s_phong.set("light.linear", 0.2f);
+	s_phong.set("light.quadratic", 0.06f);
+	s_phong.set("light.cutOff", glm::cos(glm::radians(30.0f)));
+	s_phong.set("light.outerCutOff", glm::cos(glm::radians(35.0f)));
+
+    s_phong.set("material.shininess", 32);
+
+    s_phong.set("lightSpaceMatrix", lightSpaceMatrix);
+
+    glActiveTexture(GL_TEXTURE2);
+    glBindTexture(GL_TEXTURE_2D, fbo_texture);
+    s_phong.set("depthMap", 2);
+
+    renderScene(s_phong, quad, cube, sphere, t_albedo, t_specular, s_light, view, proj);
+
+    //glBindFramebuffer(GL_FRAMEBUFFER, 0);
+    //glViewport(0, 0, Window::instance()->getWidth(), Window::instance()->getHeight());
+    //glClear(GL_COLOR_BUFFER_BIT);
+    //glDisable(GL_DEPTH_TEST);
+
+    //s_debug.use();
+    //glActiveTexture(GL_TEXTURE0);
+    //glBindTexture(GL_TEXTURE_2D, fbo_texture);
+    //s_debug.set("depthMap", 0);
+
+    //quad.render();
+}
+//-------------------------------------------------------------------FUNCI�N RENDER-------------------------------------------------------------------//
+
+//----------------------------------------------------------------FUNCI�N MAIN----------------------------------------------------------------//
+int main(int, char* []) {
+	Window* window = Window::instance();
+
+	glClearColor(0.6f, 0.6f, 0.6f, 1.0f);														// Color de la ventana	
+
+    const Shader s_phong("../projects/EJ14_01/phong.vs", "../projects/EJ14_01/blinn.fs");		// Carga del shader de phong
+    const Shader s_depth("../projects/EJ14_01/depth.vs", "../projects/EJ14_01/depth.fs");		// Carga del shader de depth
+    const Shader s_debug("../projects/EJ14_01/debug.vs", "../projects/EJ14_01/debug.fs");		// Carga del shader de debug
+    const Shader s_light("../projects/EJ14_01/light.vs", "../projects/EJ14_01/light.fs");		// Carga del shader de light
+    const Texture t_albedo("../assets/textures/bricks_albedo.png", Texture::Format::RGB);		// Textura albedo
+    const Texture t_specular("../assets/textures/bricks_specular.png", Texture::Format::RGB);	// Textura specular
+    const Quad quad(2.0f);																		// Creación del quad
+    const Cube cube(1.0f);																		// Creación del cubo
+    const Sphere sphere(1.0f, 25, 25);															// Creación de esfera
+
+    auto fbo = createFBO();
+
+    glEnable(GL_CULL_FACE);
+    glCullFace(GL_BACK);
+
+    Input::instance()->setKeyPressedCallback(onKeyPress);
+    Input::instance()->setMouseMoveCallback(onMouseMoved);
+    Input::instance()->setScrollMoveCallback(onScrollMoved);
+
+	// Mientras la ventana siga abierta
+	while (window->alive()) {
+		const float currentFrame = glfwGetTime();												// Se obtiene el frame actual
+		const float deltaTime = currentFrame - lastFrame;										// Se calcula el tiempo transcurrido
+		lastFrame = currentFrame;																// Se guarda el �ltimo frame
+
+		handleInput(deltaTime);
+        render(quad, cube, sphere, s_phong, s_depth, s_debug, s_light, t_albedo, t_specular, fbo.first, fbo.second);	// Se renderiza
+        window->frame();																		// Se dibuja la ventana
+	}
+
+    glDeleteFramebuffers(1, &fbo.first);
+    glDeleteTextures(1, &fbo.second);
+
+    return 0;
+}
+//----------------------------------------------------------------FUNCI�N MAIN----------------------------------------------------------------//
+
+////////////////////////////////////////////////////////////////////FIN DE CÓDIGO////////////////////////////////////////////////////////////////////
